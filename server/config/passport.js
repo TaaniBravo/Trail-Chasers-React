3de44606a1128c9c8740c25b9d9677ad57211848
Taani Maama <taani1maama@live.com>
2021-05-14 11:40:30 -0700

    Custom passport is created.

---
+++ server/config/passport.js
@@ -0,0 +1,26 @@
+// Declare our dependencies
+const { User } = require("../models");
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const access = require("./options")("access");
+
+// Options for JWT authentication method
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: access
+};
+
+const strategy = new JwtStrategy(options, async (payload, done) => {
+  try {
+    const user = await User.findOne({ username: payload.username });
+
+    // IF user is found
+    if (user) return done(null, user);
+
+    return done(null, false);
+  } catch (error) {
+    // IF error then return the error and user is false.
+    return done(error, false);
+  }
+});
