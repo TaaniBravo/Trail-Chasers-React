e33dc33ad60ea6b236e20558595dde590ab4256c
Taani Maama <taani1maama@live.com>
2021-05-30 15:21:11 -0700

    Finished the authControllers.

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -41,5 +41,41 @@ module.exports = {
     } catch (error) {
       res.status(422).json(error);
     }
+  },
+
+  // Login function
+  login: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+
+      // Look for user by username/email
+      const user = await User.findOne({
+        $or: [{ username: username }, { email: email }]
+      }).select("username email password");
+
+      // IF the user login fails then send back error message.
+      if (!user) return res.json({ default: "Username/Email is invalid." });
+
+      // Check if the password for the user and the password on file match.
+      const comparingPasswords = await comparePassword(password, user.password);
+
+      //   IF the the passwords don't match.
+      if (!comparingPasswords) {
+        // THEN RETURN JSON message.
+        return res.json({ default: "Password is invalid." });
+      } else {
+        // ELSE create a JWT token
+        const createToken = await jwt.issueToken(user);
+        // and return it in JSON.
+        return res.status(200).json(createToken);
+      }
+    } catch (error) {
+      res.status(422).json(error);
+    }
+  },
+
+  //   Authentication for the user.
+  authenticated: async (req, res) => {
+    res.json(req.user);
   }
 };
